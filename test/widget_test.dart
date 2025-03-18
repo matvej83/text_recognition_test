b8303bacffe34849cd7c27f37f548affa4f4a908
Vladimir Matveenko <matvej83@gmail.com>
2025-03-18 18:35:55 +0200

    widget test modified

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,25 +5,21 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:text_recognition_test/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('App test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // // Verify that our counter starts at 0.
-    // expect(find.text('0'), findsOneWidget);
-    // expect(find.text('1'), findsNothing);
-    //
-    // // Tap the '+' icon and trigger a frame.
-    // await tester.tap(find.byIcon(Icons.add));
-    // await tester.pump();
-    //
-    // // Verify that our counter has incremented.
-    // expect(find.text('0'), findsNothing);
-    // expect(find.text('1'), findsOneWidget);
+    await tester.tap(find.byIcon(Icons.text_fields));
+    await tester.pump();
+    await tester.tap(find.byIcon(Icons.credit_card));
+    await tester.pump();
+    await tester.tap(find.byIcon(Icons.credit_card_sharp));
+    await tester.pump();
   });
 }
